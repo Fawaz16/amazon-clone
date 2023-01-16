fc7ba4db26eb238102497813585755c448fe5684
Fawaz16 <99909801+Fawaz16@users.noreply.github.com>
2023-01-16 08:31:24 +0100

    k

---
+++ email.php
@@ -0,0 +1,5 @@
+ <?php
+mail('fawazrufai5@gmail.com', $_POST['subject'], $_POST['message']);
+?> 
+
+
--- index.js
+++ index.js
@@ -1,3 +1,24 @@
+
+const first = document.getElementById('first')
+const second = document.getElementById('second')
+const third = document.getElementById('third')
+
+
+
+function next(current){
+  if(current ==='first'){
+    first.style.display='none';
+    second.style.display='block';
+}
+  else if(current ==='second'){
+    second.style.display='none';
+    third.style.display='block';
+}
+}
+
+
+
+
 function revealPassword() {
     var checkbox = document.getElementById("revealPasswordCheckbox");
     var password = document.getElementById("password");
